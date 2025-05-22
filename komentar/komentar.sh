ba6ab057dc8c4b9a7a78d172edb1a9bf5ff72fb2
ahmadwafii11 <wafiahmad950@gmail.com>
2025-05-22 22:42:28 +0700

    update learn java-script komentar

---
+++ komentar/komentar.sh
@@ -0,0 +1,3 @@
+1) Komentar merupakan kode program yang tidak akan dieksekusi ketika dibaca
+2) Komentar biasanya digunakan untuk sebagai informasi tambahan atau petunjuk.
+3) Di JavaScript, kita dapat menambahkan kode komentar
